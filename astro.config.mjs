eefa23d051d096e73b89ab462d3596012a8e225d
ThePixelPolygon <ThePixelPolygon@users.noreply.github.com>
2023-07-23 12:04:39 -0400

    More visa work!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import remarkToc from 'remark-toc';
-
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkToc],
+    remarkPlugins: [remarkToc]
   },
   integrations: [preact(), tailwind({
-    applyBaseStyles: false,
-  })]
+    applyBaseStyles: false
+  }), mdx()]
 });
